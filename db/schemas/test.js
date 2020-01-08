60af6d44823a0290eb524a08cd12711af1ed8fce
Leo Nesfield <leo@thelmgn.com>
2020-01-08 12:12:14 +0000

    :fire: bye

--- db/schemas/test.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize) {
-	return sequelize.define("test", {
-		userId: Sequelize.STRING,
-		description: Sequelize.TEXT
-	})
-}
--- static/nodarkreader.js
+++
@@ -1,9 +0,0 @@
-function removeDR() {
-  for (var dr of document.querySelectorAll(".darkreader")
-  ) {
-    dr.remove()
-  }
-}
-removeDR()
-setInterval(removeDR)
-setTimeout(removeDR,1000)
